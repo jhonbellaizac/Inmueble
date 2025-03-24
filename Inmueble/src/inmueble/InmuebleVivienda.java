03e46325f2a00a2efd3648fbcfab215714a36ed2
jhonbellaizac <janerb123@gmail.com>
2025-03-24 12:19:30 -0500

    añadi los atributos

---
+++ Inmueble/src/inmueble/InmuebleVivienda.java
@@ -0,0 +1,31 @@
+package inmueble;
+public class InmuebleVivienda {
+
+protected int numeroHabitaciones;
+protected int numeroBaños;
+
+    public InmuebleVivienda(int numeroHabitaciones, int numeroBaños) {
+        this.numeroHabitaciones = numeroHabitaciones;
+        this.numeroBaños = numeroBaños;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
